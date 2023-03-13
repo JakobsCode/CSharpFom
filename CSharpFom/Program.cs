6238713cb13e0138559534d46c390f24eb4479f3
Jakob Reh <jakob.reh@gmx.de>
2023-03-13 15:20:48 +0100

    update

--- CSharpFom/Program.cs
+++ CSharpFom/Program.cs
@@ -2,11 +2,14 @@
 {
     internal class Program
     {
+        static int count = 0;
         static int Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-
             return 0;
         }
+
+        private int _j;
+        private int _i;
     }
 }
